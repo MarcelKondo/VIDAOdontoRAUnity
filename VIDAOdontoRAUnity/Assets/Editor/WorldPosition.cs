770927405347079e3738b4d13ccc4ad5c6736c35
DESKTOP-4L50FM5\Marcel <mkkondo@gmail.com>
2018-12-14 15:46:21 -0200

    Created TrackableObject.cs and TCPTestClient.cs

---
+++ VIDAOdontoRAUnity/Assets/Editor/WorldPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class DebugMenu
+{
+    [MenuItem("Debug/Print Global Position")]
+    public static void PrintGlobalPosition()
+    {
+        if (Selection.activeGameObject != null)
+        {
+            Debug.Log(Selection.activeGameObject.name + " is at " + Selection.activeGameObject.transform.position);
+        }
+    }
+    [MenuItem("Debug/Print Global Rotation")]
+    public static void PrintGlobalRotation()
+    {
+        if (Selection.activeGameObject != null)
+        {
+            Debug.Log(Selection.activeGameObject.name + " has rotation " + Selection.activeGameObject.transform.rotation);
+        }
+    }
+}
---
+++ VIDAOdontoRAUnity/Assets/Scripts/TrackableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackableObject : MonoBehaviour {
+
+    // Marker responsible for defining the system's origin
+    public Transform OriginMarker;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Pos: " + RelMakerPos().ToString());
+        print("Rot: " + RelMarkerRot().ToString());
+    }
+
+
+    // Gets the position of the object relative to the Origin Marker
+    Vector3 RelMakerPos()
+    {
+        Vector3 Pos, temp;
+        temp = OriginMarker.position - transform.position;
+
+        Pos.x = Vector3.Project(temp, OriginMarker.right).magnitude;
+        if (Vector3.Dot(temp, OriginMarker.right) > 0) Pos.x *= -1;
+
+        Pos.y = Vector3.Project(temp, OriginMarker.up).magnitude;
+        if (Vector3.Dot(temp, OriginMarker.up) > 0) Pos.y *= -1;
+
+        Pos.z = Vector3.Project(temp, OriginMarker.forward).magnitude;
+        if (Vector3.Dot(temp, OriginMarker.forward) > 0) Pos.z *= -1;
+
+        return Pos;
+    }
+    // Gets the rotation of the object relative to the Origin Marker
+    Quaternion RelMarkerRot()
+    {
+        return Quaternion.FromToRotation(OriginMarker.forward, transform.forward);
+    }
+}
